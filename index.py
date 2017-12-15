104668f2c849104804489e74140d4d1549c6a11e
Sigurduraxel2 <34171836+Sigurduraxel2@users.noreply.github.com>
2017-12-15 03:52:40 +0000

    Update index.py

--- index.py
+++ index.py
@@ -12,39 +12,28 @@ def buid():
     conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='1007002630', passwd='axelerawesome12',
                            db='1007002630_veflokaverk')
     cur = conn.cursor()
-    info = request.query.get("info")
+    heiti = request.query.get("heiti")
     val = request.query.get("val")
-    id = request.query.get("id")
-    if val == "eyda":
-        redirect("/eyda")
-        
-    elif val == "baeta":
-        cur.execute("Insert into todo (info, stada) values('{}','{:d}')".format(info, 0))
+    nr = request.query.get("nr")
+    if val == "baeta":
+        cur.execute("Insert into todo (heiti, stada) values('{}','{:d}')".format(heiti, 0))
         #cur.execute("INSERT INTO todo (heiti,stada) Values(heiti,0)")
         conn.commit()
         cur.close()
         conn.close()
-        
     elif val == "breyta":
-        cur.execute("Update todo set stada='{}' where info='{}'".format(id, info))
+        cur.execute("Update todo set stada='{}' where heiti='{}'".format(nr, heiti))
         conn.commit()
         cur.close()
         conn.close()
-    return template('buid', info = info)
+    else:
+        cur.execute("Delete from todo where heiti = '{}'".format(heiti))
+        conn.commit()
+        cur.close()
+        conn.close()
+    return template('buid', heiti = heiti)
 
 
-@route("eyda")
-def eyda():
-    id = request.query.get('id')
-    conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='1007002630', passwd='axelerawesome12',
-                           db='1007002630_veflokaverk')
-    cur.execute("SELECT todo FROM todo  where id = {}".format(id))
-    conn.commit()
-    cur.close()
-    conn.close()
-    redirect("/")
-    
-    
 @route('/')
 def todo():
     conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='1007002630', passwd='axelerawesome12',
@@ -55,7 +44,6 @@ def todo():
     #conn.close()
 
     return template('index', cur = data)
-
 
 run(host="0.0.0.0", port=os.environ.get('PORT'))
 
